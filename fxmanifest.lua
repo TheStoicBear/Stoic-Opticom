c502435721fe4df80d5f219b22eb4a126c08202e
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2024-01-18 15:06:16 -0800

    init re-release.

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+author "TheStoicBear"
+description "Stoic-Opticom"
+version "2.0.0"
+
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+client_script "source/client.lua"
+server_script "source/server.lua"
